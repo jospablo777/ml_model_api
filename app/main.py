515b63ee8099a5dea92ce4ed00f3089d77a19e8c
jospablo777 <jospablo777@gmail.com>
2024-12-19 19:19:48 -0600

    Started working in the app

--- app/main.py
+++ app/main.py
@@ -1,1 +1,9 @@
-# FastAPI
+import uvicorn
+from fastapi import FastAPI
+from app.models.bike_sharing import BikeSharingRequest
+from catboost import CatBoostRegressor
+
+
+app = FastAPI()
+ml_model = CatBoostRegressor()
+ml_model.load_model('models/catboost_model_19Dec2024.cbm')
