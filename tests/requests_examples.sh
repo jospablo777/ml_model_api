6463b5d21d0a09aaae0c088e28a9beadd3fdaa68
jospablo777 <jospablo777@gmail.com>
2024-12-22 01:33:28 -0600

    Added example

--- tests/requests_examples.sh
+++ tests/requests_examples.sh
@@ -57,3 +57,6 @@ curl -X POST "http://127.0.0.1:8000/predict"     -H "Content-Type: application/j
     "windspeed": 0.21
   }
 ]'
+
+# Empty list
+curl -X POST "http://127.0.0.1:8000/predict"     -H "Content-Type: application/json"     -d '[]'
