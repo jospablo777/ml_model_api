bba76bb3c489fe0bc1a15d962e4b81e5238cdbb6
jospablo777 <jospablo777@gmail.com>
2024-12-23 16:24:20 -0600

    Don't trust my memory

--- memory_refresher.sh
+++ memory_refresher.sh
@@ -1,7 +1,10 @@
 # I use this file to keep track of important commands to be run in the terminal for manual testing (so I don't forget).
 
 # Starts a Uvicorn server and points it to the FastAPI application object named app
-uvicorn app.main:app
+uvicorn app.main:app --host 127.0.0.1 --port 8000
+
+# Reloads the server upon changes in the app code
+uvicorn app.main:app --reload --host 127.0.0.1 --port 8000
 
 # Documents the current libraries in the venv and their versions, then store that into requirements.txt
 pip freeze > requirements.txt
